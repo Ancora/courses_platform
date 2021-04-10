44680be4bd92a8f55babb2efe40e44bdd988b752
Ancora <anderson.rocha@ancora-ti.com.br>
2021-04-10 19:15:24 -0300

    Ajuste defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,7 +32,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(256);
 
         /* Observer */
         Lesson::observe(LessonObserver::class);
